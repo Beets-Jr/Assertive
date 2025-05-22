33812752af4bedb966945885ef2ff5eea84c7859
Thales Euler <thaleslevs@gmail.com>
2025-05-22 18:54:29 -0300

    melhor visualização da hero e efeito de transparencia na header

--- JS/sobreNos.js
+++ JS/sobreNos.js
@@ -1,4 +1,13 @@
 /* Parte Thales */
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY <= 50) {  // Ajusta o valor conforme seu layout
+        header.classList.add('header-transparent');
+    } else {
+        header.classList.remove('header-transparent');
+    }
+});
 
 /* end parte Thales */
 
