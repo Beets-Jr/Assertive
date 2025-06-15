4d875456a91605dcdfba00481fb11cc2b31e87a2
Guilherme Fernandes Rezende Santos <130177734+guisantoxxx@users.noreply.github.com>
2025-06-15 17:58:34 -0300

    Update home.js

--- JS/home.js
+++ JS/home.js
@@ -66,3 +66,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
   elements.forEach(el => io.observe(el));
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+    const videoContainer = document.querySelector(['.video']);
+
+    [
+        {top: '5rem', left: '4%'},
+        {bottom: '5rem', left: '6%'},
+        {top: '10rem', left: '20%'},
+        {top: '7rem', right: '12%'},
+        {bottom: '7rem', right: '7%'},
+    ].forEach((item) => {
+        const circle = document.createElement('div');
+        circle.classList.add('video-background-circle');
+        Object.entries(item).forEach(([key, value]) => {
+            circle.style[key] = value;
+        })
+
+        videoContainer.appendChild(circle);
+    });
+})
